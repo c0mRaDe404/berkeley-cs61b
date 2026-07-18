b34c209f27bc4e167720c903fc57fcbeab541c10
Bhuvanesh <bbhuvan160@gmail.com>
2026-07-18 11:06:15 +0530

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,15 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Find the next collatz number
+     * @param n an integer
+     * @return integer
+     * */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0){
+            return (n / 2);
         } else {
-            return n * 2;
+            return (3 * n + 1);
         }
     }
 
