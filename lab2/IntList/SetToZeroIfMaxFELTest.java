abb9264003a7bc06c4575ed322d7736d037ebe28
Bhuvanesh <bbhuvan160@gmail.com>
2026-07-26 14:37:23 +0530

    tiny fix (added a few test cases just in case)

--- lab2/IntList/SetToZeroIfMaxFELTest.java
+++ lab2/IntList/SetToZeroIfMaxFELTest.java
@@ -10,6 +10,22 @@ public class SetToZeroIfMaxFELTest {
         IntList L = IntList.of(1, 22, 15);
         IntListExercises.setToZeroIfMaxFEL(L);
         assertEquals("1 -> 0 -> 15", L.toString());
+
+        L = IntList.of(55, 22, 15);
+        IntListExercises.setToZeroIfMaxFEL(L);
+        assertEquals("0 -> 0 -> 15", L.toString());
+
+        L = IntList.of(44, 22, 11);
+        IntListExercises.setToZeroIfMaxFEL(L);
+        assertEquals("0 -> 0 -> 0", L.toString());
+
+        L = IntList.of(1, 21, 11);
+        IntListExercises.setToZeroIfMaxFEL(L);
+        assertEquals("1 -> 21 -> 0", L.toString());
+
+        L = IntList.of(1, 21, 12);
+        IntListExercises.setToZeroIfMaxFEL(L);
+        assertEquals("1 -> 21 -> 12", L.toString());
     }
 
     @Test
@@ -24,5 +40,9 @@ public class SetToZeroIfMaxFELTest {
         IntList L = IntList.of(5, 535, 35, 11, 10, 0);
         IntListExercises.setToZeroIfMaxFEL(L);
         assertEquals("5 -> 0 -> 35 -> 0 -> 10 -> 0", L.toString());
+
+        L = IntList.of(5, 55, 33, 11, 9, 0);
+        IntListExercises.setToZeroIfMaxFEL(L);
+        assertEquals("5 -> 0 -> 0 -> 0 -> 0 -> 0", L.toString());
     }
 }
