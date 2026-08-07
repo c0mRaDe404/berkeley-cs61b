4604a5f3624d50fb4a15d82aeb35bfef106e143a
Bhuvanesh <bbhuvan160@gmail.com>
2026-08-07 11:45:36 +0530

    teeny tiny fix

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -162,7 +162,7 @@ public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
                 return false;
             }
 
-           for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 if (!this.get(i).equals(list.get(i))) {
                     return false;
                 }
