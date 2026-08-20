c63cf755a976558a0905034e8d980e126e538677
Bhuvanesh <bbhuvan160@gmail.com>
2026-08-20 19:20:08 +0530

    working on gitlet init command (sloppy rn)

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,8 @@
 package gitlet;
 
 import java.io.File;
+import java.io.IOException;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -26,4 +28,37 @@ public class Repository {
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
     /* TODO: fill in the rest of this class. */
+    private static final File[] DIRS = {join(GITLET_DIR,"objects"), join(GITLET_DIR,"refs"),
+            join(GITLET_DIR, "branches")};
+    private static final File[] FILES = {join(GITLET_DIR,"HEAD"), join(GITLET_DIR,"index")};
+
+    public static void createRepository() {
+        if (GITLET_DIR.exists()) {
+            System.err.println(".gitlet has already been initialized.");
+            System.exit(0);
+        }
+
+        if (!GITLET_DIR.mkdir()) {
+            System.out.println("Can't setup the" + GITLET_DIR.getPath() + " directory!");
+            System.exit(0);
+        }
+
+        for (File dir : DIRS) {
+            if (!dir.mkdir()) {
+                System.out.println("Can't setup the" + dir.getPath() + " directory!");
+                System.exit(0);
+            }
+        }
+        for (File file : FILES) {
+            try {
+                if (!file.createNewFile()) {
+                    System.out.println("Can't setup the" + file.getPath() + " file!");
+                    System.exit(0);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
 }
